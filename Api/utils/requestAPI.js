d41b5dc990400c454c4a4ab1244e530b3f493caa
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-11 16:43:00 -0300

    Criando requisição API

---
+++ Api/utils/requestAPI.js
@@ -0,0 +1,10 @@
+const fetch = require("node-fetch");
+
+const requestAPI =  async() => {
+    return await fetch('https://api.github.com/users/takenet/repos?per_page=200')
+    .then((data) => data.json()).then((data) => data)
+    .catch((err) => console.error(err))
+
+}
+
+module.exports = requestAPI;
