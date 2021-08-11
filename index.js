8821ababee2ac68404b46900d9e618c56cb961fd
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-11 16:38:38 -0300

    Finalizando estrutura

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+require('dotenv').config()
 const getLanguagesAPI = require('./Api/controllers/BotController');
 
 const app = express();
+const { PORT } = process.env;
 
 app.get('/', getLanguagesAPI);
 
-app.listen(3000, () => console.log('Servidor Online'));
+app.listen(3000, () => console.log(`Servidor Online ${ PORT }`));
