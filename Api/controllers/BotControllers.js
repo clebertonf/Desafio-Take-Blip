b1ea08f861664bdd550ed6687717ac4320ee69d9
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-12 14:37:58 -0300

    Finalizando API

--- Api/controllers/BotControllers.js
+++ Api/controllers/BotControllers.js
@@ -1,5 +1,6 @@
 const Api = require('../utils/requestAPI');
 const { notFound } = require('@hapi/boom');
+const moment = require('moment');
 const rescue = require('express-rescue');
 
 const getLanguagesAPI = rescue(async (_req, resp) => {
@@ -10,18 +11,18 @@ const getLanguagesAPI = rescue(async (_req, resp) => {
 
   const repositoriesCsharp = repositories
     .filter((repository) => repository.language === 'C#')
-    .sort((a, b) => (a.created_at < b.created_at ? -1 : a.created_at > b.created_at ? 1 : 0));
+     .sort((a, b) => (a.created_at < b.created_at ? -1 : a.created_at > b.created_at ? 1 : 0));
 
   const orderedRepositories = repositoriesCsharp.map(({
-    full_name, description, owner: { avatar_url }, ..._others
+    full_name, description, owner: { avatar_url }, created_at, ..._others
   }) => ({
     full_name,
     description,
     avatar_url,
+    date: moment(created_at).format('DD-MM-YYYY'),
   })).slice(0, 5);
 
   const objectWithRepositories = Object.assign({}, orderedRepositories);
-
 
   return resp.status(200).json(objectWithRepositories);
 });
